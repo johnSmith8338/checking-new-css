4f24341ae2c29528042d5b6457b651b8d6de8448
Dmitriy <john.smith.dm8338@gmail.com>
2025-01-14 02:36:39 +0200

    - banner template

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -1,9 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-banner',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
