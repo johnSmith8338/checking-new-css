29b44211bca741ec417cfc13b25ededac3f8c270
Dmitriy <john.smith.dm8338@gmail.com>
2025-01-14 02:05:45 +0200

    - add footer and content components with styles; update header and app layout

---
+++ src/app/components/banner/banner.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-banner',
+  standalone: true,
+  imports: [],
+  templateUrl: './banner.component.html',
+  styleUrl: './banner.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BannerComponent { }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
